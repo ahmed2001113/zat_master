49fb441abb06b8d782d261d0106fa3926ce0a8b7
Mohab Elbasiry <61328117+mohabahmed333@users.noreply.github.com>
2023-09-04 03:10:07 +0300

    remove static pages from product

--- pages/product/[product].jsx
+++ pages/product/[product].jsx
@@ -249,24 +249,24 @@ export async function getStaticProps({params}){
       }
 }
 
-export async function  getStaticPaths(){
+// export async function  getStaticPaths(){
 
-    const {data} = await client.query({
-        query:ProductPathsQuery
-     });
-     const products = data?.products?.nodes;
+//     const {data} = await client.query({
+//         query:ProductPathsQuery
+//      });
+//      const products = data?.products?.nodes;
 
 
-    const paths = products.map(product=>{
+//     const paths = products.map(product=>{
 
-        return{
-            params:{product:product.id}
-        }
-    })
+//         return{
+//             params:{product:product.id}
+//         }
+//     })
 
-return{
-    paths,
-    fallback:false
-}
+// return{
+//     paths,
+//     fallback:false
+// }
 
-}
+// }
