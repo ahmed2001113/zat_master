783cc83f8c9326e88c9540a9d32344e6615175f6
Mohab Elbasiry <61328117+mohabahmed333@users.noreply.github.com>
2023-09-09 19:56:19 +0300

    adding headers "

--- src/utls/apolloConfigrations/client-config.js
+++ src/utls/apolloConfigrations/client-config.js
@@ -1,7 +1,7 @@
 const clientConfig = {
 
     siteUrl:process.env.PUBLIC_WEBSITE_URL,
-    graphqlUrl:`http://zat98.com/graphql`
+    graphqlUrl:`https://zat98.com/graphql`
 }
 
 export default clientConfig
